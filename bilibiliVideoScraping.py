25b63281b3afcf009611f0b7491c2671af7def85
AnimeSam <sphung@uncc.edu>
2020-06-26 15:04:50 -0400

    Implemented quiet option

--- bilibiliVideoScraping.py
+++ bilibiliVideoScraping.py
@@ -16,6 +16,7 @@ import pathlib
 import re
 import argparse
 import time
+import urllib.request
 
 
 # Final Version 2
@@ -23,10 +24,44 @@ import time
 #TODO split user url and turn it into usable url(i.e.remove everything after '?' mark)
 #Sites with error: https://space.bilibili.com/654295/video?tid=0&page=1&keyword=&order=pubdate, https://space.bilibili.com/1726310?from=search&seid=12136548391520811086
 # Learn about try, except and raise
+#TODO allow user to choose pages to scrape
+#TODO Decide if this script will support homepage scraping
+#TODO work on allow user to scrape a page with filtered settings
+
+#Tested Links:
+    #a.https://space.bilibili.com/1726310/video
+    #b.https://space.bilibili.com/654295/video 
+
+    #c.https://search.bilibili.com/all?keyword=%E3%82%A2%E3%83%8B%E3%82%B2%E3%83%A9%EF%BC%81%E3%83%87%E3%82%A3%E3%83%89%E3%82%A5%E3%83%BC%E3%83%BC%E3%83%B3
+    #d.https://search.bilibili.com/all?keyword=%E9%8A%80%E9%AD%82%E6%96%87%E5%AD%97%E8%B5%B7%E3%81%93%E3%81%97
+    #e.https://search.bilibili.com/all?keyword=%E3%81%8A%E9%A3%9F%E4%BA%8B%E5%87%A6%E3%81%BE%E3%81%A4%E3%81%8A%E3%81%8B&from_source=nav_search_new
+        # gets "'from_source' is not recognized as an internal or external command, operable program or batch file." error but everything works due to to not getting full link
+    #f.https://search.bilibili.com/all?keyword=no%20radio%20no%20life&from_source=nav_search_new
+        # gets "'from_source' is not recognized as an internal or external command, operable program or batch file." error but everything works
+
+
+    #h.https://space.bilibili.com/216970/channel/detail?cid=13217
+    #i.https://space.bilibili.com/216970/channel/detail?cid=13214
+
+#Untested Links:
+    #Home pages
+    #j.https://space.bilibili.com/942021/video
+    #g.https://space.bilibili.com/391100
+
+
+#Works: a,b,c,d,e,f,h,i
+
 
 def make_soup(driver): 
-    #response = requests.get(url)
+    url = driver.current_url
+    response = requests.get(url)
+    # headers = {}
+    # headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+    # response = urllib.request.Request(url, headers=headers)
+    #print(response.status_code)
+    
     soup = BeautifulSoup(driver.page_source, 'html.parser')
+    #soup = BeautifulSoup(response.content, 'html.parser')
     return soup
 
 
@@ -192,151 +227,181 @@ def find_last_page(driver):
         print("Error: Can't get the last page")
         raise Exception
 
+try:
+    try: 
+        #TODO 
+        #try catch PermissionError 
+        #Catch invalid urls
 
-try: 
-    #TODO 
-    #try catch PermissionError 
-    #Catch invalid urls
 
+        defaultPath = pathlib.Path().absolute()
+        print("Default Save Location:", defaultPath, "\n")
+        parser = argparse.ArgumentParser(prog='biliurls')
 
-    defaultPath = pathlib.Path().absolute()
-    print("Default Save Location:", defaultPath, "\n")
-    parser = argparse.ArgumentParser(prog='biliurls')
+        # Arguments
+        parser.add_argument('-n', '--name',
+                            type=str,
+                            metavar='',
+                            default='output',
+                            help="Name of the csv file")
 
-    # Arguments
-    parser.add_argument('-n', '--name',
-                        type=str,
-                        metavar='',
-                        default='output',
-                        help="Name of the csv file")
+        parser.add_argument('-d', '--driver',
+                            type=str,
 
-    parser.add_argument('-d', '--driver',
-                        type=str,
-                        metavar='',
-                        nargs='+',
-                        default=defaultPath,
-                        help="Absolute web driver path")
+                            nargs='+',
+                            default=defaultPath,
+                            help="Absolute web driver path")
 
-    parser.add_argument('-p', '--path',
-                        type=str,
-                        metavar='',
-                        nargs='+',
-                        default=defaultPath,
-                        help="The user's chosen absolute path for the csv file")
-    
-    parser.add_argument('-l', '--link',
-                        type=str,
-                        metavar='',
-                        required=True,
-                        help="Link to extract video urls")
+        parser.add_argument('-s', '--save',
+                            type=str,
 
-    parser.add_argument('-w', '--wait',
-                        type=int,
-                        metavar='',
-                        help="The amount of second to stop and wait for browser to load. Recommended for use only if browser is taking longer than 2 seconds to load. Prevent links from being extracted on same page as the next page has yet to load.")                    
-    
-    parser.add_argument('-v', '--verbose',
-                        action='store_true',
-                        help="Show more information as the command executes")
-
-    args = parser.parse_args()
-    
-    link = args.link
-    if args.wait is not None:
-        wait = args.wait
-    else:
-        wait = 2
-
-    INVALID_CHARACTERS_RE =  re.compile(r"^[^<>/{}[\]~`]*$")
-    if INVALID_CHARACTERS_RE.match(args.name):
-        if args.name is not None:
-            defaultFile = os.path.abspath(str(args.path) + "\\" + str(args.name)) 
-            print(defaultFile)
-        else:
-             defaultFile = os.path.abspath(str(defaultPath) + "\\" + str(args.name)) 
-             print(defaultFile)  
-    else:
-        print("Error: illegal characters detected in file name or save path")
-        exit()
+                            nargs='+',
+                            default=defaultPath,
+                            help="The user's chosen absolute save path for the csv file")
         
+        parser.add_argument('-l', '--link',
+                            type=str,
+                            metavar='',
+                            required=True,
+                            help="Link to extract video urls")
 
+        parser.add_argument('-w', '--wait',
+                            type=int,
+                            metavar='',
+                            help="The amount of second to stop and wait for browser to load. Recommended for use only if browser is taking longer than 2 seconds to load. Prevent links from being extracted on same page as the next page has yet to load.")                    
+        
+        parser.add_argument('-q', '--quiet',
+                            action='store_false',
+                            help="Show more information as the command executes")
 
-    print(os.path.join(str(args.driver),"\\chromedriver.exe"))
-    with open(defaultFile + ".csv", 'w', newline='') as csv_file:
-            csv_writer = writer(csv_file)
+        # parser.add_argument('-p', '--page',
+        #                     action='store_true',
+        #                     help="Select specific page(s) to scrape")
 
-            #url = input("URL: ")
+        args = parser.parse_args()
+        
+        link = args.link
+        if args.wait is not None:
+            wait = args.wait
+        else:
+            wait = 2
 
-            #DRIVER_PATH = r"C:\Users\samph\AppData\Local\Programs\Python\Python37-32\Python Files\WebScraping\chromedriver.exe"
-
-            if args.driver is not None:
-                driver = webdriver.Chrome(os.path.abspath(str(args.driver) + r"\\chromedriver.exe"))
-                print(os.path.abspath(str(defaultPath) + "\\chromedriver.exe"))
-            else: 
-                driver = webdriver.Chrome(os.path.abspath(str(defaultPath) + r"\\chromedriver.exe"))
-                print(os.path.abspath(str(defaultPath) + "\\chromedriver.exe"))
-
-            #driver = webdriver.Chrome(os.path.abspath(str(defaultPath) + r"\\WebScraping\chromedriver.exe"))
-
-            driver.get(link)
-
-            soup = make_soup(driver)
-
-            lastPage = find_last_page(driver)
-
-            print("Total Pages: " + str(lastPage))
-            page = 1
-            pageExtension = ''
+        INVALID_CHARACTERS_RE =  re.compile(r"^[^<>/{}[\]~`]*$")
+        if INVALID_CHARACTERS_RE.match(args.name):
+            if args.name is not None:
+                defaultFile = os.path.abspath(str(args.save) + "\\" + str(args.name)) 
+                if args.quiet:
+                    print(defaultFile)
+            else:
+                defaultFile = os.path.abspath(str(defaultPath) + "\\" + str(args.name)) 
+                if args.quiet:
+                    print(defaultFile)  
+        else:
+            if args.quiet:
+                print("Error: illegal characters detected in file name or save path")
+            exit()
             
-            if link.find('channel') == -1 and lastPage > 1:  
-                print("\n" + 'Page:',1)
-                scrape_url(driver)
-                while page < lastPage:
-                    page = page + 1
 
+        # create variable = open(defaultFile + ".csv", 'w', newline='') so try except, we can use variable.close()
+        #f = open('/pythonwork/thefile_subset1.csv', 'w')
+        #writer = csv.writer(f)
+        #f.close()
+        print(os.path.join(str(args.driver),"\\chromedriver.exe"))
+        with open(defaultFile + ".csv", 'w', newline='') as csv_file:
+                csv_writer = writer(csv_file)
 
-                    if soup.find_all(class_='h-inner') != [] and link.find('space') == -1:
-                        pageExtension = '&page='+str(page)
-                        print(link+pageExtension)
-                    else:
-                        pageExtension = '?page='+str(page)
-                        print(link+pageExtension)
-                    
-                    # pageExtension = '?page='+str(page)
+                #url = input("URL: ")
 
-                    fullUrl = link+pageExtension
-                    driver.get(fullUrl)
-                    soup = make_soup(driver)
-                    time.sleep(2)
+                #DRIVER_PATH = r"C:\Users\samph\AppData\Local\Programs\Python\Python37-32\Python Files\WebScraping\chromedriver.exe"
+
+                if args.driver is not None:
+                    driver = webdriver.Chrome(os.path.abspath(str(args.driver) + r"\\chromedriver.exe"))
+                    if args.quiet:
+                        print(os.path.abspath(str(defaultPath) + "\\chromedriver.exe"))
+                else: 
+                    driver = webdriver.Chrome(os.path.abspath(str(defaultPath) + r"\\chromedriver.exe"))
+                    if args.quiet:
+                        print(os.path.abspath(str(defaultPath) + "\\chromedriver.exe"))
+
+                #driver = webdriver.Chrome(os.path.abspath(str(defaultPath) + r"\\WebScraping\chromedriver.exe"))
+
+                driver.get(link)
+                soup = make_soup(driver)
+                lastPage = find_last_page(driver)
+                if args.quiet:
+                    print("Total Pages: " + str(lastPage))
+                page = 1
+                pageExtension = ''
+                
+                if link.find('channel') == -1 and lastPage >= 1:  
+                    if args.quiet:
+                        print("\n" + 'Page:',1)
                     scrape_url(driver)
+                    while page < lastPage:
+                        page = page + 1
 
-            # In the event that the webpage is a single page application
-            else:     
-                try:
-                    i = 1
-                    h = 1 
-                    print("\n" + 'Page:',1)
-                    # Loops through all video urls through all pages    
-                    while i <= lastPage:
-                        for video in soup.find_all('a', class_='cover cover-normal'):    
-                            link = video['href'].replace('//','https://')
-                            csv_writer.writerow([link])
-                            print(str(h)+'.' + link)
-                            h = h + 1
-                        if i < lastPage:
-                            driver.find_element_by_class_name('be-pager-next').click()
-                            print("\n" + 'Page:',i + 1)
-                            #TODO Find a better way than using sleep
-                            time.sleep(wait)
 
-                        i = i + 1
+                        if soup.find_all(class_='h-inner') != [] and link.find('space') == -1 or link.find('keyword') > -1:
+                            pageExtension = '&page='+str(page)
+                            if args.quiet:
+                                print(link+pageExtension)
+                        else:
+                            pageExtension = '?page='+str(page)
+                            if args.quiet:
+                                print(link+pageExtension)
+                        
+                        # pageExtension = '?page='+str(page)
+
+                        fullUrl = link+pageExtension
+                        driver.get(fullUrl)
+
+                        #Sleep(2) allows all urls to be scraped
+                        # Assume the sleep allows driver to get all of page's source code  before soup initializes
+                        # since without it not everypage will get scraped 
+                        time.sleep(1)
+
+                        #driver.set_page_load_timeout(10)
+                        #driver.delete_all_cookies()
+                        
                         soup = make_soup(driver)
-                    print("\n" + 'Extracted:',h-1 ,'URLS')           
-                except:
-                    raise Exception
+                        scrape_url(driver)
 
-            # Close all windows
-            driver.quit()
-except KeyboardInterrupt as ke:
-    print('\nUser cancelled the operation')
+                # In the event that the webpage is a single page application
+                else:     
+                    try:
+                        i = 1
+                        h = 1 
+                        if args.quiet:
+                            print("\n" + 'Page:',1)
+                        # Loops through all video urls through all pages    
+                        while i <= lastPage:
+                            for video in soup.find_all('a', class_='cover cover-normal'):    
+                                link = video['href'].replace('//','https://')
+                                csv_writer.writerow([link])
+                                if args.quiet:
+                                    print(str(h)+'.' + link)
+                                h = h + 1
+                            if i < lastPage:
+                                driver.find_element_by_class_name('be-pager-next').click()
+                                if args.quiet:
+                                    print("\n" + 'Page:',i + 1)
+                                #TODO Find a better way than using sleep
+                                time.sleep(wait)
+
+                            i = i + 1
+                            soup = make_soup(driver)
+                        if args.quiet:    
+                            print("\n" + 'Extracted:',h-1 ,'URLS')           
+                    except:
+                        raise Exception
+
+                # Close all windows
+                driver.quit()
+    except KeyboardInterrupt as ke:
+        if args.quiet:
+            print('\nUser cancelled the operation')
+        driver.quit()
+except Exception:
+    if args.quiet:
+        print("An unexpected error has occurred!")
     driver.quit()
